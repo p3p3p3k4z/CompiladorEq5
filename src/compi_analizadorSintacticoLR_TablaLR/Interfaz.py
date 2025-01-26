869a0b118aab4b8db571b1813f37a1cece63fd69
Mario Ramirez <ragm030119@gs.utm.mx>
2025-01-25 22:20:55 -0600

    correcciones

--- src/compi_analizadorSintacticoLR_TablaLR/Interfaz.py
+++ src/compi_analizadorSintacticoLR_TablaLR/Interfaz.py
@@ -7,14 +7,14 @@ from logica import *
 def InterfazTablaAS():
     font1=("Times New Roman",14)
     VentanaPrincipal =Toplevel()
-    VentanaPrincipal.title("Algoritmo de la colección canónica")
+    VentanaPrincipal.title("Tabla de analisis sintactico LR")
     try:
         VentanaPrincipal.state("zoomed")
     except:
         VentanaPrincipal.attributes('-zoomed', True) 
-    VentanaPrincipal.config(background="#F9C0AB")
+    VentanaPrincipal.config(background="#15B392")
     font2=("Times New Roman",20)
-    archivoLabel=Label(VentanaPrincipal,text="Seleccionar Archivo:",font=font1,width=20,background="#A8CD89",foreground="white")
+    archivoLabel=Label(VentanaPrincipal,text="Seleccionar Archivo:",font=font1,width=20,background="#D2FF72",foreground="white")
     archivoLabel.place(x=60,y=30)
     frameGramatica=Frame(VentanaPrincipal,width=400,height=300)
     frameGramatica.place(x=20,y=100)
@@ -22,11 +22,11 @@ def InterfazTablaAS():
     frameTablaAS.place(x=350,y=100)
     frameSiguientes=Frame(VentanaPrincipal,width=400,height=300)#Aqui va el frame de siguientes
     frameSiguientes.place(x=20,y=400)#Aqui va el frame de siguientes
-    archivoButton=Button(VentanaPrincipal,text="Abrir archivo",width=20,command=lambda:abrirArchivo(VentanaPrincipal,frameGramatica,frameSiguientes),bg="#F4E0AF",font=font1)
+    archivoButton=Button(VentanaPrincipal,text="Abrir archivo",width=20,command=lambda:abrirArchivo(VentanaPrincipal,frameGramatica,frameSiguientes),bg="#6256CA",font=font1)
     archivoButton.place(x=300,y=20)
-    ImprimirResultad0s=Button(VentanaPrincipal,text="Imprimir tabla ",width=20,bg="#F4E0AF",font=font1,command=lambda:ImprimirTablaAS(frameTablaAS))
+    ImprimirResultad0s=Button(VentanaPrincipal,text="Imprimir tabla ",width=20,bg="#6256CA",font=font1,command=lambda:ImprimirTablaAS(frameTablaAS))
     ImprimirResultad0s.place(x=300,y=60)
-    limpiarButton=Button(VentanaPrincipal,text="Limpiar",width=20,bg="#F4E0AF",font=font1,command=lambda:limpiar(frameTablaAS,frameGramatica,frameSiguientes))
+    limpiarButton=Button(VentanaPrincipal,text="Limpiar",width=20,bg="#6256CA",font=font1,command=lambda:limpiar(frameTablaAS,frameGramatica,frameSiguientes))
     limpiarButton.place(x=700,y=60)
     
     VentanaPrincipal.mainloop()
